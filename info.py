da09daf594e6a9b124511ab14a74860f8f3a4a40
Alisaleh06 <83263954+Alisaleh06@users.noreply.github.com>
2021-04-27 14:25:18 +1000

    Add files via upload

---
+++ info.py
@@ -0,0 +1,40 @@
+import time
+
+print("Hello! This is a characteristics test. Answer carefully!");
+
+what = input("What word describes you? ")
+print("Exciting!");
+gender = input("What's your gender? ")
+if (gender == "male", "man", "guy"):
+    print("Same")
+else:
+    print("Nice!");
+
+hair = input("What's the colour of your hair? ")
+print("Amazing!");
+
+print("So, here is what you are:");
+print("You are described as " + what );
+print("You are a " + gender);
+print("Your hair is the colour " + hair);
+
+
+
+time.sleep(3)
+
+print("Wait a sec...")
+time.sleep(2)
+print("Updating 20%")
+time.sleep(1)
+print("Updating 40%")
+time.sleep(1)
+print("Updating 60%")
+time.sleep(1)
+print("Updating 80%")
+time.sleep(1)
+print("Update complete.")
+print("You are now gay!")
+time.sleep(1)
+print("Congratulations")
+input("Press enter to exit...")
+
